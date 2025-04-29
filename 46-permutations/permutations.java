6ba06fbff82a55320f0b0758ac362785924d036c
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-04-29 15:52:18 +0530

    Time: 1 ms (93.35%) | Memory: 44.5 MB (64.61%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,23 +1,29 @@
 class Solution {
-    private void findPermutation(int arr[], List<List<Integer>> ans, boolean map[], List<Integer> ds){
-        if(ds.size() == arr.length){
+    private void findpermute(int ind, int nums[], List<List<Integer>> ans){
+        int n = nums.length;
+        if(ind == n){
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0; i<n; i++){
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i = 0; i<arr.length; i++){
-            if(!map[i]){
-                map[i] = true;
-                ds.add(arr[i]);
-                findPermutation(arr, ans, map, ds);
-                ds.remove(ds.size() - 1);
-                map[i] = false;
-            }
+        for(int i = ind; i<n; i++){
+            swap(nums, i, ind);
+            findpermute(ind+1, nums, ans);
+            swap(nums, i, ind);
         }
+    }
+    private void swap(int arr[], int i, int j){
+        int temp = arr[i]; 
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean map[] = new boolean[nums.length];
-        findPermutation(nums, ans, map, new ArrayList<>());
+        findpermute(0, nums, ans);
         return ans;
     }
+    
 }
