6c84b87d7f4f106d00fc0fff21a74670260dee1e
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-07-05 16:23:49 +0530

    Time: 14 ms (60.15%) | Memory: 46.8 MB (89.87%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,48 +1,32 @@
 class Solution {
-    class Node{
-        int r;
-        int c;
-        int l;
-        Node(int r, int c, int l){
-            this.r = r ;
-            this.c = c;
-            this.l = l;
-        }
-    }
     public int[][] updateMatrix(int[][] mat) {
         int n = mat.length;
         int m = mat[0].length;
         int vis[][] = new int[n][m];
         int dist[][] = new int[n][m];
-        Queue<Node> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.add(new Node(i, j, 0));
+                if(mat[i][j] == 0) {
+                    q.offer(new int[] {i, j, 0});
                     vis[i][j] = 1;
-                }
-                else{
-                    vis[i][j] = 0;
-                }
+                }  
             }
         }
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
+        int dir[][] = {{-1, 0}, {0, -1}, {0,1}, {1, 0}};
         while(!q.isEmpty()){
-            int row = q.peek().r;
-            int col = q.peek().c;
-            int steps = q.peek().l;
-            q.poll();
-            dist[row][col] = steps;
+            int[] p = q.poll();
+            dist[p[0]][p[1]] = p[2];
             for(int i=0; i<4; i++){
-                int nr = row + dr[i];
-                int nc = col + dc[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc] == 0){
+                int nr = dir[i][0] + p[0];
+                int nc = dir[i][1] + p[1];
+                if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc]==0 && mat[nr][nc] == 1){
                     vis[nr][nc] = 1;
-                    q.add(new Node(nr, nc, steps+1));
+                    q.offer(new int[] {nr,nc, p[2]+1});
                 }
             }
         }
         return dist;
     }
+   
 }
