15934cc08281ffe202cd063e7893b287b145e084
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-07-08 12:28:47 +0530

    Time: 7 ms (87.02%) | Memory: 57.7 MB (41.08%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -2,27 +2,18 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int vis[][] = new int[n][m];
         int dir[][] = {{-1,0},{0,-1},{0,1},{1,0}};
         for(int j=0; j<m; j++){
             //first row
-            if(vis[0][j]==0 && grid[0][j] == 1){
-                dfs(grid, vis, dir, 0, j);
-            }
+            if(grid[0][j] == 1) dfs(grid, dir, 0, j);
             //last row
-            if(vis[n-1][j]==0 && grid[n-1][j] == 1){
-                dfs(grid, vis, dir, n-1, j);
-            }
+            if(grid[n-1][j] == 1) dfs(grid, dir, n-1, j);
         }
         for(int i=0; i<n; i++){
             //first column
-            if(vis[i][0]==0 && grid[i][0] == 1){
-                dfs(grid, vis, dir, i, 0);
-            }
+            if(grid[i][0] == 1) dfs(grid, dir, i, 0);
             //last column 
-            if(vis[i][m-1]==0 && grid[i][m-1]==1){
-                dfs(grid, vis, dir, i, m-1);
-            }
+            if(grid[i][m-1]==1) dfs(grid, dir, i, m-1);
         }
         int count = 0;
         for(int i=0; i<n; i++){
@@ -32,14 +23,13 @@ class Solution {
         }
         return count;
     }
-    private void dfs(int[][] grid, int vis[][], int dir[][], int row, int col){
-        vis[row][col] = 1;
+    private void dfs(int[][] grid, int dir[][], int row, int col){
         grid[row][col] = 0;
         for(int i=0; i<4; i++){
             int nr = row + dir[i][0];
             int nc = col + dir[i][1];
-            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && vis[nr][nc] == 0 && grid[nr][nc] == 1){
-                dfs(grid, vis, dir, nr, nc);
+            if(nr>=0 && nr<grid.length && nc>=0 && nc<grid[0].length && grid[nr][nc] == 1){
+                dfs(grid, dir, nr, nc);
             }
         }
     }
