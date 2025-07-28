c3a592747b2ac7f319b9e47d2b226b14fcec8fc3
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-07-28 18:20:08 +0530

    Time: 11 ms (89.42%) | Memory: 44.6 MB (79.78%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,16 +1,24 @@
 class Solution {
     public int numberOfSubstrings(String s) {
         int ans = 0;
-        HashMap<Character, Integer> mp = new HashMap<>();
-        mp.put('a', -1);
-        mp.put('b', -1);
-        mp.put('c', -1);
+        // HashMap<Character, Integer> mp = new HashMap<>();
+        // mp.put('a', -1);
+        // mp.put('b', -1);
+        // mp.put('c', -1);
+        int[] lastSeen = new int[3];
+        Arrays.fill(lastSeen, -1);
         for(int i=0; i<s.length(); i++){
-            mp.put(s.charAt(i), i);
-            if(mp.get('a') != -1 && mp.get('b') != -1 && mp.get('c') != -1){
-                int minIndex = Math.min(mp.get('a'), Math.min(mp.get('b'), mp.get('c')));{
-                    ans += minIndex + 1;
-                }
+            // mp.put(s.charAt(i), i);
+
+            // if(mp.get('a') != -1 && mp.get('b') != -1 && mp.get('c') != -1){
+            //     int minIndex = Math.min(mp.get('a'), Math.min(mp.get('b'), mp.get('c')));{
+            //         ans += minIndex + 1;
+            //     }
+            // }
+            lastSeen[s.charAt(i) - 'a'] = i;
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+                int minIndex=Math.min(lastSeen[0], Math.min(lastSeen[1], lastSeen[2]));
+                ans += minIndex + 1;
             }
         }
         return ans;
