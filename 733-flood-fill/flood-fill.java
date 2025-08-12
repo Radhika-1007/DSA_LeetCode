c969a2d670dfc73be4866a0dd58333161f9864f6
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-08-12 23:15:12 +0530

    Time: 1 ms (44.54%) | Memory: 45.5 MB (5.41%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -2,19 +2,17 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length, m = image[0].length;
         int ini = image[sr][sc];
-        int vis[][] = new int[n][m];
         int dir[][] = {{-1,0},{0,-1},{0,1},{1,0}};
-        dfs(image, ini, color, dir, vis, sr, sc);
+        dfs(image, ini, color, dir, sr, sc);
         return image;
     }
-    private void dfs(int[][] image, int ini, int color, int dir[][], int vis[][], int sr, int sc){
-        vis[sr][sc] = 1;
+    private void dfs(int[][] image, int ini, int color, int dir[][], int sr, int sc){
         image[sr][sc] = color;
         for(int i=0; i<4; i++){
             int nr = sr + dir[i][0], nc = sc + dir[i][1];
-            if(nr>=0 && nr<image.length && nc>=0 && nc<image[0].length && image[nr][nc] == ini && vis[nr][nc] == 0){
+            if(nr>=0 && nr<image.length && nc>=0 && nc<image[0].length && image[nr][nc] == ini && image[nr][nc] != color){
                 image[nr][nc] = color;
-                dfs(image, ini, color, dir, vis, nr, nc);
+                dfs(image, ini, color, dir, nr, nc);
             }
         }
     }
