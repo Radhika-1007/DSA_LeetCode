4c6ae614ab36f2ebf98505ac326164bb647f88c3
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-08-12 18:54:38 +0530

    Time: 2 ms (66.82%) | Memory: 42.4 MB (61.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,34 +1,39 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int time=0, fresh=0;
-        Queue<int []> q = new LinkedList<>();
+        int n = grid.length, m= grid[0].length;
+        int fresh = 0;
+        Queue<int[]> q = new LinkedList<>();
+        int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 2) q.add(new int[]{i,j});
-                if(grid[i][j] == 1) fresh++;
+                if(grid[i][j] == 2){
+                    q.add(new int[]{i, j});
+                }
+                else if(grid[i][j] == 1){
+                    fresh++;
+                }
             }
         }
-        int[][] dir = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}}; 
-        while(!q.isEmpty() && fresh !=0){
-           int len = q.size();
-           for(int i=0; i<len; i++){
-                int[] pos = q.poll();
-                int r = pos[0];
-                int c = pos[1];
+        int time = 0;
+        while(!q.isEmpty() && fresh != 0 ){
+            int len = q.size();
+            for(int i=0; i<len; i++){
+                int ro[] = q.poll();
+                int row = ro[0];
+                int col = ro[1];
                 for(int j=0; j<4; j++){
-                    int nr = r + dir[j][0];
-                    int nc = c + dir[j][1];
-                    if(nr >= 0 && nr < n && nc >=0 && nc < m && grid[nr][nc] == 1){
+                    int nr = row + dir[j][0];
+                    int nc = col + dir[j][1];
+                    if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == 1){
                         grid[nr][nc] = 2;
                         q.offer(new int[]{nr, nc});
                         fresh--;
                     }
                 }
-           }
-           time++;
+            }
+            time++;
         }
-        return (fresh == 0)? time : -1;
+        return (fresh == 0)? time: -1;
     }
+   
 }
