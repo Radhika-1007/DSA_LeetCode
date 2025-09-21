6cc8aabce0eaab7036dcda11109a8d1ad5e6f7f5
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-09-22 00:29:05 +0530

    Time: 1 ms (5.28%) | Memory: 41.3 MB (38.14%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,35 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
-         int n = nums.length;
-        if (n == 1) return nums[0]; // only one house
-
+        int n = nums.length;
+        if(n==1) return nums[0];
         ArrayList<Integer> temp1 = new ArrayList<>();
         ArrayList<Integer> temp2 = new ArrayList<>();
-
-        // create two cases: exclude first or exclude last
-        for (int i = 0; i < n; i++) {
-            if (i != 0) temp1.add(nums[i]);         // exclude first
-            if (i != n - 1) temp2.add(nums[i]);     // exclude last
+        for(int i=0; i<n; i++){
+            if(i!=0) temp1.add(nums[i]);
+            if(i!=n-1) temp2.add(nums[i]);
         }
-
         int dp1[] = new int[temp1.size()];
         int dp2[] = new int[temp2.size()];
         Arrays.fill(dp1, -1);
         Arrays.fill(dp2, -1);
-
-        int case1 = cache(temp1.size() - 1, dp1, temp1);
-        int case2 = cache(temp2.size() - 1, dp2, temp2);
-
+        int case1 = f(dp1, temp1, temp1.size()-1);
+        int case2 = f(dp2, temp2, temp2.size()-1);
         return Math.max(case1, case2);
     }
-    public int cache(int ind, int dp[], ArrayList<Integer> nums) {
-        if (ind < 0) return 0;
-        if (ind == 0) return nums.get(ind);
-        if (dp[ind] != -1) return dp[ind];
-
-        int pick = nums.get(ind) + cache(ind - 2, dp, nums);
-        int notpick = cache(ind - 1, dp, nums);
-
+    private int f(int dp[], ArrayList<Integer> nums, int ind){
+        if(ind<0) return 0;
+        if(ind==0) return nums.get(ind);
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums.get(ind) + f(dp, nums, ind-2);
+        int notpick = f(dp, nums, ind-1);
         return dp[ind] = Math.max(pick, notpick);
     }
 }
