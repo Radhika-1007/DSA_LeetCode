3b304e8216490f09ce7ebf69319730464047c37c
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-10-03 19:06:45 +0530

    Time: 4 ms (34.70%) | Memory: 44.2 MB (41.41%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nge = new HashMap<>();
+        Map<Integer, Integer> mp = new HashMap<>();
         Stack<Integer> st = new Stack<>();
-        int n = nums2.length;
-        for(int i = n-1; i>=0; i--){
-            while(!st.isEmpty() && st.peek() <= nums2[i]){
+        for(int i=nums2.length-1; i>=0; i--){
+           while(!st.isEmpty() && st.peek() <= nums2[i]){
                 st.pop();
-            }
-            int nextGreater = st.isEmpty()? -1: st.peek();
-            nge.put(nums2[i], nextGreater);
-            st.push(nums2[i]);
+           }
+           mp.put(nums2[i], st.isEmpty()? -1: st.peek());
+           st.push(nums2[i]);
         }
-        int result[] = new int[nums1.length];
-        for(int i = 0; i<result.length; i++){
-            result[i] = nge.get(nums1[i]);
+        for(int i=0; i<nums1.length; i++){
+            nums1[i] = mp.get(nums1[i]);
         }
-        return result;
+        return nums1;
     }
 }
