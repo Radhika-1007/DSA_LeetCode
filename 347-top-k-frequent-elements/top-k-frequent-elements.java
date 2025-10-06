eab6d5f2dbd33065b25e07a01677dc4adde58bd2
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-10-06 23:26:56 +0530

    Time: 14 ms (57.43%) | Memory: 48.8 MB (43.94%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freqmap = new HashMap<>();
-        // mapping frequency in hashmap
         int n = nums.length;
-        for(int num: nums){
-            freqmap.put(num, freqmap.getOrDefault(num, 0) + 1);
+        Map<Integer, Integer> mp = new HashMap<>();
+        for(int i=0; i<n; i++){
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
         }
-        // putting elements based on frequency
-        List<Integer>[] bucket = new List[n+1]; // this is array of list
-        for(int key: freqmap.keySet()){
-            int freq = freqmap.get(key);
-            if(bucket[freq] == null){
-                bucket[freq] = new ArrayList<>();
-            }
-            bucket[freq].add(key);
-        }
-        //collecting top k elements from end 
-        List<Integer> resultList = new ArrayList<>();
-        for(int i=bucket.length-1; i>=0 && resultList.size() < k; i--){
-            if(bucket[i] != null){
-                resultList.addAll(bucket[i]);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+        for(Map.Entry<Integer, Integer> entry: mp.entrySet() ){
+            pq.add(entry);
+            if(pq.size() > k){
+                pq.poll();
             }
         }
-        //list to array
         int result[] = new int[k];
-        for(int i=0; i<k; i++){
-            result[i] = resultList.get(i);
+        int i=0;
+        while(!pq.isEmpty()){
+            result[i++] = pq.poll().getKey();
         }
         return result;
     }
