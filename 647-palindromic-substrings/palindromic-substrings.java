60329a033b274bfd68883d16671b1b9baf0285e1
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-10-07 11:48:05 +0530

    Time: 6 ms (69.41%) | Memory: 41.5 MB (86.77%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,30 +1,16 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
-        if(n==1) return 1;
-        int dp[][] = new int[n][n];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-        }
         int count = 0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(i==j) count+=1;
-                else{
-                    if(isPalin(s, i, j, dp) == 1){
-                        count+=1;
-                    }
-                }
+        for(int center = 0; center < 2*n-1; center++){
+            int left = center/2;
+            int right = left + center %2;
+            while(left>= 0 && right<n && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
             }
         }
         return count;
-    }
-    private int isPalin(String s, int i, int j, int dp[][]){
-        if(i>=j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = isPalin(s, i+1, j-1, dp);
-        }
-        else return dp[i][j] = 0;
     }
 }
