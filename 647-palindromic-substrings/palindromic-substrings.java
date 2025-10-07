4e7853713f168d305ac9bd398a7fb4a928f70d97
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-10-07 11:17:05 +0530

    Time: 28 ms (24.00%) | Memory: 49.9 MB (5.11%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -21,6 +21,7 @@ class Solution {
     }
     private int isPalin(String s, int i, int j, int dp[][]){
         if(i>=j) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
         if(s.charAt(i) == s.charAt(j)){
             return dp[i][j] = isPalin(s, i+1, j-1, dp);
         }
