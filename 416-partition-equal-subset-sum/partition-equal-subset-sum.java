349832bc1b60cfa4830f2effb7e1899675d89925
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-04 16:30:53 +0530

    Time: 94 ms (21.92%) | Memory: 76 MB (5.26%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-       int n = nums.length;
-       int total = 0;
-       for(int i=0; i<n; i++){
-            total+= nums[i];
-       } 
-       if(total % 2 != 0) return false;
-       int target = total/2;
-       int dp[][] = new int[n][target+1];
-       for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-       }
-        return f(n-1, target, dp, nums);
-    }
-    private boolean f(int ind, int target, int dp[][], int nums[]){
-        if(target == 0) return true;
-        if(ind == 0) return nums[ind] == target; 
-        if(dp[ind][target] != -1) return dp[ind][target] == 1;
-        boolean nottake = f(ind-1, target, dp, nums);
-        boolean take = false;
-        if(target >= nums[ind]){
-            take = f(ind-1, target-nums[ind], dp, nums);
+        int total = 0;
+        for(int i=0; i<nums.length; i++){
+            total += nums[i];
         }
-        boolean result = nottake || take;
-        dp[ind][target] = result? 1: 0;
-        return result;
+        if(total % 2 != 0) return false;
+        int dp[][] = new int[nums.length][(total/2)+1];
+        for(int i=0; i<nums.length; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return f(nums.length-1, nums, dp, total/2);
+    }
+    public boolean f(int i, int[] nums, int dp[][], int target){
+        if(target == 0) return true;
+        if(i==0 ) return nums[i] == target;
+        if(dp[i][target] != -1) return dp[i][target] == 1;
+        boolean nottake = f(i-1, nums, dp, target);
+        boolean take = false;
+        if(nums[i] <= target){
+            take = f(i-1, nums, dp, target - nums[i]);
+        }
+        dp[i][target] = take || nottake ? 1: 0; 
+        return take || nottake;
     }
 }
