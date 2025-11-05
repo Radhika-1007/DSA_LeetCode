735e5f313c513cc5ea63c0ba825528df420b830a
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-05 17:55:24 +0530

    Time: 8 ms (48.81%) | Memory: 65.2 MB (5.03%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -18,7 +18,10 @@ class Solution {
         }
     }
     public boolean isPalindrome(String s){
-        StringBuilder reversed = new StringBuilder(s).reverse();
-        return s.equals(reversed.toString());
+       int left = 0, right = s.length()-1;
+       while(left < right){
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+       }
+       return true;
     }
 }
