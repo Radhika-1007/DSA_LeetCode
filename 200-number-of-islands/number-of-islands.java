b49bf090eee98fcfb61e7cb2a691077789d29ae8
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-06 14:53:59 +0530

    Time: 4 ms (48.82%) | Memory: 51.9 MB (28.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,49 +1,27 @@
 class Solution {
-    class Pair{
-        int first;
-        int second;
-        Pair(int f, int s){
-            first = f;
-            second = s;
-        }
-    }
-    private void bfs(char[][] grid, int vis[][], int row, int col){
-        int n = grid.length;
-        int m = grid[0].length;
-        Queue<Pair> q =  new LinkedList<Pair>();
-        vis[row][col] = 1;
-        q.add(new Pair(row, col));
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, 1, 0, -1};
-        while(!q.isEmpty()){
-            int ro = q.peek().first;
-            int co = q.peek().second;
-            q.poll();
-            for(int i=0; i<4; i++){
-                int nr = ro + drow[i];
-                    int nc = co + dcol[i];
-                    if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && vis[nr][nc] == 0){
-                        vis[nr][nc] = 1;
-                        q.add(new Pair(nr, nc));
-                    }        
-                
-            }
-        }    
-
-    }
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int count = 0;
+        int n = grid.length, m = grid[0].length;
+        int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
         int vis[][] = new int[n][m];
+        int count = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(vis[i][j] == 0 && grid[i][j] == '1'){
-                    bfs(grid, vis, i, j);
                     count++;
+                    dfs(vis, i, j, grid, dir);
                 }
             }
         }
         return count;
+    }
+    public void dfs(int vis[][], int row, int col, char grid[][], int dir[][]){
+        vis[row][col] = 1;
+        for(int i=0; i<4; i++){
+            int nr = row + dir[i][0];
+            int nc = col + dir[i][1];
+            if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == '1' && vis[nr][nc] == 0){
+                dfs(vis, nr, nc, grid, dir);
+            }
+        }
     }
 }
