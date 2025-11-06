a3440e72df61ae75d7ad3a59b8b28c9aafc6c3fc
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-06 15:44:42 +0530

    Time: 1 ms (90.37%) | Memory: 43.3 MB (12.25%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,20 +1,24 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
         int n = s1.length(), m = s2.length();
+        if(n+m < s3.length()) return false;
         int dp[][] = new int[n+1][m+1];
-        if(m+n != s3.length()) return false;
-        for(int i=0; i<=n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return f(s1, s2, s3, 0, 0, 0, dp);
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
+        return f(s1, s2, s3, dp, 0, 0, 0);
     }
-    public boolean f(String s1, String s2, String s3, int i, int j, int k, int dp[][]){
-        if(k==s3.length()) return (i==s1.length() && j==s2.length());
+    public boolean f(String s1, String s2, String s3, int dp[][], int i, int j, int k){
+        if(k==s3.length()) return (i==s1.length()) && (j==s2.length());
         if(dp[i][j] != -1) return dp[i][j] == 1;
         boolean res = false;
-        if(i<s1.length() && s1.charAt(i) == s3.charAt(k)) res = f(s1, s2, s3, i+1, j, k+1, dp);
-        if(!res && j<s2.length() && s2.charAt(j) == s3.charAt(k)) res = f(s1, s2, s3, i, j+1, k+1, dp);
-        dp[i][j] = res? 1: 0;
+        if(i<s1.length() && k<s3.length() && s1.charAt(i) == s3.charAt(k)){
+            res = f(s1, s2, s3, dp, i+1, j, k+1);
+        }
+         if(j<s2.length() && k<s3.length() && s2.charAt(j) == s3.charAt(k) && !res){
+            res = f(s1, s2, s3, dp, i, j+1, k+1);
+        }
+        dp[i][j] = res? 1:0;
         return res;
     }
+
+
 }
