efebbf4f83e4baab55acc73d4d15d8a15df4c3e8
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-07 12:47:17 +0530

    Time: 2 ms (67.17%) | Memory: 44 MB (8.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,21 +1,16 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length, m= grid[0].length;
-        int fresh = 0;
-        Queue<int[]> q = new LinkedList<>();
+        int n = grid.length , m = grid[0].length;
         int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+        Queue<int []> q = new LinkedList<>();
+        int fresh =0, time = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 2){
-                    q.add(new int[]{i, j});
-                }
-                else if(grid[i][j] == 1){
-                    fresh++;
-                }
+                if(grid[i][j] == 2) q.add(new int[]{i, j});
+                else if(grid[i][j] == 1) fresh++;
             }
         }
-        int time = 0;
-        while(!q.isEmpty() && fresh != 0 ){
+        while(!q.isEmpty() && fresh != 0){
             int len = q.size();
             for(int i=0; i<len; i++){
                 int ro[] = q.poll();
@@ -26,14 +21,13 @@ class Solution {
                     int nc = col + dir[j][1];
                     if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == 1){
                         grid[nr][nc] = 2;
-                        q.offer(new int[]{nr, nc});
+                        q.add(new int[]{nr, nc});
                         fresh--;
                     }
                 }
             }
             time++;
         }
-        return (fresh == 0)? time: -1;
+        return (fresh != 0) ? -1 : time;
     }
-   
 }
