aa3cb849a16b2ddada7678fd392ab0e202ad44aa
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-19 14:08:04 +0530

    Time: 7 ms (49.91%) | Memory: 47.6 MB (5.04%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -3,29 +3,27 @@ class Solution {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0; i<numCourses; i++) adj.add(new ArrayList<>());
         for(int it[]: prerequisites){
-            adj.get(it[1]).add(it[0]);
+            adj.get(it[0]).add(it[1]);
         }
-        
-        int indegree[] = new int[numCourses];
+        int indegree[] = new int [numCourses];
         for(int i=0; i<numCourses; i++){
             for(int it: adj.get(i)){
                 indegree[it]++;
             }
-        }
+        } 
+        ArrayList<Integer> topo = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0)q.add(i);
+            if(indegree[i] == 0) q.offer(i);
         }
-        ArrayList<Integer> topo = new ArrayList<>();
         while(!q.isEmpty()){
             int node = q.poll();
             topo.add(node);
             for(int it: adj.get(node)){
                 indegree[it]--;
-                if(indegree[it] == 0) q.add(it);
+                if(indegree[it] == 0) q.offer(it);
             }
         }
-        if(topo.size() == numCourses) return true;
-        return false;
+        return topo.size() == numCourses;
     }
 }
