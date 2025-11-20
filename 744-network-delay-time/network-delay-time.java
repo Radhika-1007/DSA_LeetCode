c07e73bf55ddb133b5ca2500778de1fdbc3fb896
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-20 22:48:00 +0530

    Time: 10 ms (82.62%) | Memory: 49.2 MB (41.31%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,34 +1,32 @@
 class Pair{
-    int first, second;
-    Pair(int f, int s){
-        first = f;
-        second = s;
+    int node, time;
+    Pair(int n, int t){
+        node = n;
+        time = t;
     }
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
         for(int i=0; i<=n; i++) adj.add(new ArrayList<>());
-        for(int time[] : times){
+        for(int time[]: times){
             adj.get(time[0]).add(new Pair(time[1], time[2]));
         }
-        PriorityQueue<Pair> pq= new PriorityQueue<>((x,y) -> x.second-y.second);
         int dist[] = new int[n+1];
         for(int i=0; i<=n; i++){
             dist[i] = Integer.MAX_VALUE;
         }
         dist[k] = 0;
-        pq.add(new Pair(k,0));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> x.time - y.time);
+        pq.offer(new Pair(k, 0));
         while(!pq.isEmpty()){
-            Pair curr = pq.poll();
-            int node = curr.first;
-            int time = curr.second;
-            for(Pair it: adj.get(node)){
-                int adjNode = it.first;
-                int weight = it.second;
-                if(time + weight < dist[adjNode]){
-                    dist[adjNode] = time + weight;
-                    pq.add(new Pair(adjNode, dist[adjNode]));
+            int no = pq.peek().node;
+            int t = pq.peek().time;
+            pq.poll();
+            for(Pair it: adj.get(no)){
+                if(t + it.time < dist[it.node]){
+                    dist[it.node] = t + it.time;
+                    pq.offer(new Pair(it.node, dist[it.node]));
                 }
             }
         }
