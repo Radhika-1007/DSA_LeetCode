77b2cbb9ccf4b1088fb9c633e6c477af0084bc31
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-11-30 15:15:22 +0530

    Time: 23 ms (39.13%) | Memory: 54 MB (23.59%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,10 +5,10 @@ class Solution {
         for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
         return f(n-1, m-1, text1, text2, dp);
     }
-    private int f(int i, int j, String text1, String text2, int dp[][]){
+    public int f(int i, int j, String text1, String text2, int dp[][]){
         if(i<0 || j<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(text1.charAt(i) == text2.charAt(j)) return dp[i][j] = 1 + f(i-1, j-1, text1, text2, dp);
-        return dp[i][j] = Math.max(f(i-1, j, text1, text2, dp), f(i, j-1, text1, text2, dp));
+        if(text1.charAt(i) == text2.charAt(j)) return dp[i][j] =  1 + f(i-1, j-1, text1, text2, dp);
+        return dp[i][j] = Math.max(f(i-1, j, text1, text2, dp),f(i, j-1, text1, text2, dp));
     }
 }
