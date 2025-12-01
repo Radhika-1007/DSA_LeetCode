6d2bfa29b9275878dda94c7e0fe517fc8de11044
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-12-01 13:03:13 +0530

    Time: 82 ms (30.61%) | Memory: 75.6 MB (12.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -5,24 +5,22 @@ class Solution {
         for(int i=0; i<n; i++){
             total += nums[i];
         }
-        if(total % 2 != 0) return false;
         int target = total/2;
+        if(total % 2 != 0) return false;
         int dp[][] = new int[n][target+1];
-        for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return f(n-1, nums, dp, target);
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return f(n-1, target, nums, dp);
     }
-    private boolean f(int i, int nums[], int dp[][], int target){
+    private boolean f(int i, int target, int nums[], int dp[][]){
         if(target == 0) return true;
         if(i==0) return nums[0] == target;
         if(dp[i][target] != -1) return dp[i][target] == 1;
-        boolean nottake = f(i-1, nums, dp, target);
+        boolean nottake = f(i-1, target, nums, dp);
         boolean take = false;
         if(nums[i] <= target){
-            take = f(i-1, nums, dp, target - nums[i]);
+            take = f(i-1, target-nums[i], nums, dp);
         }
-        boolean result = (take || nottake);
+        boolean result = take || nottake;
         dp[i][target] = result? 1: 0;
         return result;
     }
