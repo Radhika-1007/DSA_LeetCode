a582024be9648f772ecbe5975e537f02fe2c6280
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-12-10 13:49:14 +0530

    Time: 23 ms (94.07%) | Memory: 64.1 MB (17.90%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.java
@@ -5,16 +5,15 @@ class Solution {
         for(int i=0; i<n; i++){
             Arrays.fill(dp[i], -1);
         }
-        return f(0, n-1, dp, s);
+        return f(0, n-1, s, dp);
     }
-    private int f(int i, int j, int dp[][], String s){
+    private int f(int i, int j, String s, int dp[][]){
         if(i>j) return 0;
         if(i==j) return 1;
         if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)){ 
-        return dp[i][j] = 2+ f(i+1, j-1, dp, s);
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + f(i+1, j-1, s, dp);
         }
-        return dp[i][j] = Math.max(f(i+1, j, dp, s), f(i, j-1, dp, s));
-
+        return dp[i][j] = Math.max(f(i+1, j, s, dp), f(i, j-1, s, dp));
     }
 }
