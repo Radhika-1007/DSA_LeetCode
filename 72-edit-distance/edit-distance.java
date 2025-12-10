8f5c0a0f96892f7c1372ae98d788f058a3ce4d75
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-12-10 14:48:15 +0530

    Time: 3 ms (96.01%) | Memory: 47 MB (48.81%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -5,12 +5,13 @@ class Solution {
         for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
         return f(n-1, m-1, word1, word2, dp);
     }
-    private int f(int i, int j, String word1, String word2, int dp[][]){
+    private int f(int i, int j, String s, String t, int dp[][]){
         if(i<0) return j+1;
         if(j<0) return i+1;
         if(dp[i][j] != -1) return dp[i][j];
-        if(word1.charAt(i) == word2.charAt(j)) return dp[i][j] =  0 + f(i-1, j-1, word1, word2, dp);
-        return dp[i][j] = 1 + Math.min(f(i, j-1, word1, word2, dp), Math.min(f(i-1, j, word1, word2, dp), f(i-1, j-1, word1, word2, dp)));
-
+        if(s.charAt(i) == t.charAt(j)){
+            return dp[i][j] = f(i-1, j-1, s, t, dp);
+        }
+        return dp[i][j] = 1 + Math.min(f(i-1, j-1, s, t, dp), Math.min(f(i-1, j, s, t, dp), (f(i, j-1, s, t, dp))));
     }
 }
