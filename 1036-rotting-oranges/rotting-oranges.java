82df490014d297f49d49713ea4f5c6c0d9d42aed
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-12-29 23:24:11 +0530

    Time: 2 ms (89.04%) | Memory: 44.1 MB (53.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,33 +1,32 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length , m = grid[0].length;
+        int n = grid.length, m = grid[0].length;
         int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
         Queue<int []> q = new LinkedList<>();
-        int fresh =0, time = 0;
+        int fresh = 0, totaltime = 0;
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                if(grid[i][j] == 2) q.add(new int[]{i, j});
+                if(grid[i][j] == 2) q.offer(new int[]{i, j});
                 else if(grid[i][j] == 1) fresh++;
             }
         }
-        while(!q.isEmpty() && fresh != 0){
+        while(!q.isEmpty() && fresh!=0){
             int len = q.size();
             for(int i=0; i<len; i++){
                 int ro[] = q.poll();
-                int row = ro[0];
-                int col = ro[1];
-                for(int j=0; j<4; j++){
-                    int nr = row + dir[j][0];
-                    int nc = col + dir[j][1];
+                int row = ro[0], col = ro[1];
+                for(int d=0; d<4; d++){
+                    int nr = row + dir[d][0];
+                    int nc = col + dir[d][1];
                     if(nr>=0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == 1){
                         grid[nr][nc] = 2;
-                        q.add(new int[]{nr, nc});
+                        q.offer(new int[] {nr, nc});
                         fresh--;
                     }
                 }
             }
-            time++;
+            totaltime++;
         }
-        return (fresh != 0) ? -1 : time;
+        return (fresh==0)?totaltime: -1;
     }
 }
