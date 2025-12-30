a93f4c6a19350b6720652cf33e5f507ab2cdc0ea
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2025-12-31 00:19:04 +0530

    Time: 5 ms (98.53%) | Memory: 47.1 MB (6.94%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -1,49 +1,17 @@
-class Pair{
-    int f;
-    int s;
-    Pair(int f, int s){
-        this.f = f;
-        this.s = s;
-    }
-}
-class Tuple{
-    int first;
-    int second;
-    int third;
-    Tuple(int f, int s, int t){
-        first = f;
-        second = s;
-        third = t;
-    }
-}
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
-        for(int flight[] : flights){
-            adj.get(flight[0]).add(new Pair(flight[1], flight[2]));
-        }
-        Queue<Tuple> q = new LinkedList<>();
         int dist[] = new int[n];
-        for(int i=0; i<n; i++) dist[i] = Integer.MAX_VALUE;
-        q.offer(new Tuple(0, src, 0)); // stops, node, dist
+        Arrays.fill(dist, Integer.MAX_VALUE);
         dist[src] = 0;
-        while(!q.isEmpty()){
-            Tuple curr = q.poll();
-            int stops = curr.first;
-            int node = curr.second;
-            int price = curr.third;
-            if(stops > k) continue;
-            for(Pair it: adj.get(node)){
-                int adjNode = it.f;
-                int adjPrice = it.s;
-                if(adjPrice + price < dist[adjNode] && stops <= k){
-                    dist[adjNode] = adjPrice + price;
-                    q.add(new Tuple(stops+1, adjNode, adjPrice + price));
+        for(int i=0; i<=k; i++){
+            int temp[] = dist.clone();
+            for(int flight[]: flights){
+                if(dist[flight[0]] != Integer.MAX_VALUE && dist[flight[0]] + flight[2] < temp[flight[1]]){
+                    temp[flight[1]] = dist[flight[0]] + flight[2];
                 }
             }
+            dist = temp;
         }
-        if(dist[dst] == Integer.MAX_VALUE) return -1;
-        return dist[dst];
+        return (dist[dst] != Integer.MAX_VALUE)? dist[dst] : -1;
     }
 }
