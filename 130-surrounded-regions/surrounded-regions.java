9d6b3c9f0bb8bc10e82e6abd82265fd0217815b3
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-02 18:48:38 +0530

    Time: 1 ms (99.75%) | Memory: 47.4 MB (90.21%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,48 +1,44 @@
 class Solution {
     public void solve(char[][] board) {
-        int n = board.length;
-        int m = board[0].length;
+        int n = board.length, m = board[0].length;
         int vis[][] = new int[n][m];
         int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        //for first and last row
-       for(int j = 0; j<m; j++){
+        for(int j=0; j<m; j++){
             //first row
             if(vis[0][j] == 0 && board[0][j] == 'O'){
-                dfs(vis, board, 0, j, dir);
+                dfs(0, j, board, vis, dir);
             }
             //last row
             if(vis[n-1][j] == 0 && board[n-1][j] == 'O'){
-                dfs(vis, board, n-1, j, dir);
+                dfs(n-1, j, board, vis, dir);
             }
-       }
-       for(int i = 0; i<n; i++){
+        }
+        for(int i=0; i<n; i++){
             //first column
             if(vis[i][0] == 0 && board[i][0] == 'O'){
-                dfs(vis, board, i, 0, dir);
+                dfs(i, 0, board, vis, dir);
             }
             //last column
             if(vis[i][m-1] == 0 && board[i][m-1] == 'O'){
-                dfs(vis, board, i, m-1, dir);
-            }
-       }
-       for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(vis[i][j]==0){
-                board[i][j]='X';
-            }
-            else{
-                board[i][j]='O';
+                dfs(i, m-1, board, vis, dir);
             }
         }
-       }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j] == 1 && board[i][j] == 'O'){
+                    board[i][j] = 'O';
+                }
+                else board[i][j] = 'X';
+            }
+        }
     }
-    private void dfs(int vis[][], char[][] board, int r, int c, int dir[][]){
-        vis[r][c] = 1;
-        for(int i=0; i<4; i++){
-            int nr = dir[i][0] + r;
-            int nc = dir[i][1] + c;
-            if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && board[nr][nc] == 'O' && vis[nr][nc] == 0){
-                dfs(vis, board, nr, nc, dir);
+    public void dfs(int i, int j, char board[][], int vis[][], int dir[][]){
+        vis[i][j] = 1;
+        for(int d=0; d<4; d++){
+            int nr = i + dir[d][0];
+            int nc = j + dir[d][1];
+            if(nr>=0 && nr<board.length && nc>=0 && nc<board[0].length && vis[nr][nc] == 0 && board[nr][nc] == 'O'){
+                dfs(nr, nc, board, vis, dir);
             }
         }
     }
