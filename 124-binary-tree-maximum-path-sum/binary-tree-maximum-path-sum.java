d6ad4a23e6a05505f39a6c06a5d1e23d1673fb09
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-14 12:23:25 +0530

    Time: 1 ms (30.19%) | Memory: 46.5 MB (89.42%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -23,7 +23,7 @@ class Solution {
         if(root == null) return 0;
         int left = Math.max(0, maxPath(root.left));
         int right = Math.max(0, maxPath(root.right));
-        maxValue = Math.max(maxValue, left+right+root.val);
+        maxValue = Math.max(maxValue, left + right + root.val);
         return Math.max(left, right) + root.val;
     }
 }
