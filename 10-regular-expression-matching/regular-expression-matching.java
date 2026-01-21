c22a849f5b01e522240edaba3dbef7dbd372f5f8
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-21 15:39:14 +0530

    Time: 1 ms (99.67%) | Memory: 43.8 MB (32.18%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        boolean dp[][] = new boolean[s.length()+1][p.length()+1];
+        Boolean dp[][] = new Boolean[s.length()+1][p.length()+1];
         return f(0, 0, s, p, dp);
     }
-    public boolean f(int i, int j, String s, String p, boolean dp[][]){
+    public boolean f(int i, int j, String s, String p, Boolean dp[][]){
         int n = s.length(), m = p.length();
         if(j==m) return i==n;
+        if(dp[i][j] != null) return dp[i][j];
         boolean match = (i<n && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
         boolean ans;
         if(j+1<m && p.charAt(j+1) == '*'){
