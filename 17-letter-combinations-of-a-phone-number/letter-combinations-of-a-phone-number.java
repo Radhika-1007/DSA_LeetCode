b159259e256dd27cbc727b579f51576a50afd270
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-30 00:25:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (73.67%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -2,10 +2,10 @@ class Solution {
     public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         String map[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        backtrack(0, map, digits, res, new StringBuilder());
+        backtrack(0, map, res, new StringBuilder(), digits);
         return res;
     }
-    public void backtrack(int index, String map[], String digits, List<String> res, StringBuilder path){
+    public void backtrack(int index, String map[], List<String> res, StringBuilder path, String digits){
         if(index == digits.length()){
             res.add(path.toString());
             return;
@@ -13,7 +13,7 @@ class Solution {
         String letters = map[digits.charAt(index) - '0'];
         for(char ch: letters.toCharArray()){
             path.append(ch);
-            backtrack(index+1, map, digits, res, path);
+            backtrack(index+1, map, res, path, digits);
             path.deleteCharAt(path.length()-1);
         }
     }
