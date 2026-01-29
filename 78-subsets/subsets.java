4e43d99b18eafcb07fa47ad00ab93f1b04508f69
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-29 14:28:41 +0530

    Time: 1 ms (88.08%) | Memory: 44.4 MB (30.88%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        f(nums, 0, ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        f(0, nums, res, new ArrayList<>());
+        return res;
     }
-    public void f(int[] nums, int i, List<List<Integer>> ans, List<Integer> curr){
-        if(i>=nums.length){
-            ans.add(new ArrayList<>(curr));
+    public void f(int i, int nums[], List<List<Integer>> res, List<Integer> path){
+        if(i == nums.length){
+            res.add(new ArrayList<>(path));
             return;
         }
-        curr.add(nums[i]);
-        f(nums, i+1, ans, curr);
-        curr.remove(curr.size()-1);
-        f(nums, i+1, ans, curr);
-
+        path.add(nums[i]);
+        f(i+1, nums, res, path);
+        path.remove(path.size()-1);
+        f(i+1, nums, res, path);
     }
 }
