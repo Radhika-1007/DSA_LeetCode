00fc57091431cb5aa3ca4c954e40ba50afc0d045
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-30 11:14:17 +0530

    Time: 117 ms (22.87%) | Memory: 44.7 MB (26.81%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -2,8 +2,7 @@ class Solution {
     public int maxCoins(int[] nums) {
         int n = nums.length;
         int arr[] = new int[n+2];
-        arr[0] = 1;
-        arr[arr.length-1] = 1;
+        arr[0] = 1; arr[arr.length-1] = 1;
         for(int i=1; i<arr.length-1; i++){
             arr[i] = nums[i-1];
         }
@@ -15,9 +14,9 @@ class Solution {
     }
     public int f(int i, int j, int arr[], int dp[][]){
         if(i>j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
         int cost;
         int max = Integer.MIN_VALUE;
-        if(dp[i][j] != -1) return dp[i][j];
         for(int ind = i; ind<=j; ind++){
             cost = arr[i-1] * arr[ind] * arr[j+1] + f(i, ind-1, arr, dp) + f(ind+1, j, arr, dp);
             max = Math.max(max, cost);
