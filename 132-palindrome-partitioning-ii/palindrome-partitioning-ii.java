0c120616a5e4cabae3c5b249dcdd59926783f173
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-01-31 11:50:18 +0530

    Time: 2519 ms (13.78%) | Memory: 46.8 MB (63.00%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -2,24 +2,28 @@ class Solution {
     public int minCut(String s) {
         int n = s.length();
         int dp[] = new int[n+1];
-        dp[n] = 0;
-        for(int i=n-1; i>=0; i--){
-            int min = Integer.MAX_VALUE;
-            for(int j=i; j<n; j++){
-                if(isPalindrome(i, j, s)){
-                    int cut = 1 + dp[j+1];
-                    min = Math.min(min, cut);
-                }
+        Arrays.fill(dp, -1);
+        return f(0, s, dp, n) - 1;
+    }
+    public int f(int i, String s, int dp[], int n){
+        if(i==n) return 0;
+        if(dp[i] != -1) return dp[i];
+        StringBuilder temp = new StringBuilder("");
+        int min = Integer.MAX_VALUE;
+        for(int j=i; j<n; j++){
+            temp.append(s.charAt(j));
+            if(isPalindrome(i, j, s)){
+                int cut = 1 + f(j+1, s, dp, n);
+                min = Math.min(cut, min);
             }
-            dp[i] = min;
         }
-        return dp[0] - 1;
+        dp[i] = min;
+        return dp[i];
     }
     public boolean isPalindrome(int i, int j, String s){
         while(i<j){
             if(s.charAt(i) != s.charAt(j)) return false;
-            i++;
-            j--;
+            i++; j--;
         }
         return true;
     }
