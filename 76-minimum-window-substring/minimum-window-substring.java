76e871b7c8aa84f4ac8ba0b7c0c10f62624b35f1
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-02-13 11:11:55 +0530

    Time: 3 ms (75.98%) | Memory: 45.4 MB (90.47%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,25 +1,23 @@
 class Solution {
     public String minWindow(String s, String t) {
         int n = s.length(), m = t.length();
-        int hash[] = new int[256];
-        int left = 0, right = 0, cut = 0, minLen = Integer.MAX_VALUE, sLen = -1;
-        for(int i=0; i<m; i++){
-            hash[t.charAt(i)]++;
-        }
-        while(right < n){
-            if(hash[s.charAt(right)]  > 0) cut++;
+        int hash[] = new int[128];
+        for(int i=0; i<m; i++) hash[t.charAt(i)]++;
+        int left=0, right=0, minLen=Integer.MAX_VALUE, cut=0, sLen=-1;
+        while(right<n){
+            if(hash[s.charAt(right)] > 0) cut++;
             hash[s.charAt(right)]--;
             while(cut == m){
-                if((right - left + 1) < minLen) {
+                if(right-left+1 < minLen){
                     minLen = right-left+1;
                     sLen = left;
-                }    
+                }
                 hash[s.charAt(left)]++;
                 if(hash[s.charAt(left)] > 0) cut--;
                 left++;
             }
             right++;
         }
-        return sLen == -1? "" : s.substring(sLen, sLen + minLen);
+        return (sLen == -1)? "": s.substring(sLen, sLen+minLen);
     }
 }
