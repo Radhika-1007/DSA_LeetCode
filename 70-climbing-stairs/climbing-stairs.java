a308c66059662800e67170883d968ee4b5aed398
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-01 19:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (15.80%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -4,9 +4,9 @@ class Solution {
         Arrays.fill(dp, -1);
         return f(n, dp);
     }
-    private int f(int i, int dp[]){
-        if(i==0) return 1;
+    public int f(int i, int dp[]){
         if(i<0) return 0;
+        if(i==0) return 1;
         if(dp[i] != -1) return dp[i];
         int oneStep = f(i-1, dp);
         int twoStep = f(i-2, dp);
