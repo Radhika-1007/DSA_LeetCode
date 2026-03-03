d2a83bebdb8044ded2c6aa30b2cf2c647cc5745c
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-03 16:16:28 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (36.05%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -7,10 +7,9 @@ class Solution {
     }
     public int f(int i, int nums[], int dp[]){
         if(i<0) return 0;
-        if(i==0) return nums[0];
         if(dp[i] != -1) return dp[i];
-        int take = nums[i] + f(i-2, nums, dp);
         int nottake = f(i-1, nums, dp);
+        int take = nums[i] + f(i-2, nums, dp);
         return dp[i] = Math.max(take, nottake);
     }
 }
