70f8288feb90b05ff343c1427c8ec8added02fba
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-04 01:17:29 +0530

    Time: 5 ms (97.23%) | Memory: 43 MB (70.41%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,14 +1,15 @@
 class Solution {
     int n;
     public int countSubstrings(String s) {
-        n = s.length(); int  count = 0;
+        n = s.length();
+        int count = 0;
         for(int i=0; i<n; i++){
-            count += f(s, i, i);
-            count += f(s, i, i+1);
+            count += f(i, i, s);
+            count += f(i, i+1, s);
         }
         return count;
     }
-    public int f(String s, int l, int r){
+    public int f(int l, int r, String s){
         int res = 0;
         while(l>=0 && r<n && s.charAt(l) == s.charAt(r)){
             res++;
