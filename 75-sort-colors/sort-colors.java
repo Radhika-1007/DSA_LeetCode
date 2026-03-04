d3636250dc83e09f99842cf145da9c8a896431fc
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-04 23:09:04 +0530

    Time: 1 ms (24.40%) | Memory: 43.5 MB (78.34%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        Map<Integer, Integer> mp = new HashMap<>();
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
+        }
+        int index = 0;
+        for(int i=0; i<=2; i++){
+            int count = mp.getOrDefault(i, 0);
+            while(count-- > 0){
+                nums[index++] = i;
+            }
+        }
     }
 }
