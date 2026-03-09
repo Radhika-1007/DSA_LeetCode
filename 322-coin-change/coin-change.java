1853e0c96569be771515e47b3c214840f2425afe
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-09 21:08:32 +0530

    Time: 38 ms (10.33%) | Memory: 47.6 MB (11.93%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,20 +1,22 @@
 class Solution {
+    int n;
+    int dp[][];
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int dp[][] = new int[n][amount+1];
+        n = coins.length;
+        dp = new int[n][amount+1];
         for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
-        int res = f(n-1, amount, coins, dp);
+        int res = f(n-1, coins, amount);
         return (res == (int)1e8)? -1: res;
     }
-    public int f(int i, int amount, int coins[], int dp[][]){
-        if(amount == 0) return 0;
-        if(i<0) return (int)1e8; 
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int nottake = f(i-1, amount, coins, dp);
-        int take = (int)1e8; 
-        if(coins[i] <=amount){
-            take = 1 + f(i, amount - coins[i], coins, dp);
+    private int f(int i, int coins[], int target){
+        if(target == 0) return 0;
+        if(i<0) return (int)1e8;
+        if(dp[i][target] != -1) return dp[i][target];
+        int nottake = f(i-1, coins, target);
+        int take = (int)1e8;
+        if(coins[i] <= target){
+            take = 1 + f(i, coins, target-coins[i]);
         }
-        return dp[i][amount] = Math.min(take , nottake);
+        return dp[i][target] = Math.min(take, nottake);
     }
 }
