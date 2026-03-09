9d6f6347c081c8a3e7802b1bfcdc3ff2328ec231
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-09 20:42:44 +0530

    Time: 24 ms (77.42%) | Memory: 48.8 MB (69.25%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -5,8 +5,8 @@ class Solution {
         int sum = 0, count = 0;
         for(int num: nums){
             sum += num;
-            if(mp.containsKey(sum - k)) count += mp.get(sum-k);
-            mp.put(sum, mp.getOrDefault(sum, 0)+1);
+            if(mp.containsKey(sum - k)) count += mp.get(sum - k);
+            mp.put(sum, mp.getOrDefault(sum , 0) +1 );
         }
         return count;
     }
