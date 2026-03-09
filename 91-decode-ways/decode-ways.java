0c85b43b4194e442e6f6cfc8a2f8889ebc9049d0
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-09 17:32:12 +0530

    Time: 1 ms (86.28%) | Memory: 43.3 MB (51.57%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,19 +1,21 @@
 class Solution {
+    int n;
+    int dp[];
     public int numDecodings(String s) {
-        int dp[] = new int[s.length()];
+        n = s.length();
+        dp = new int[n+1];
         Arrays.fill(dp, -1);
-        return f(0, s, dp);
+        return f(0, s);
     }
-    private int f(int i, String s, int dp[]){
-        int n = s.length();
+    private int f(int i, String s){
         if(i==n) return 1;
         if(s.charAt(i) == '0') return 0;
         if(dp[i] != -1) return dp[i];
-        int ways = f(i+1, s, dp);
-        if(i+1<n){
-            int twodigit = Integer.parseInt(s.substring(i, i+2));
-            if(twodigit >= 10 && twodigit <= 26){
-                ways += f(i+2, s, dp);
+        int ways = f(i+1, s);
+        if(i+1 < n){
+            int twoDigit = Integer.parseInt(s.substring(i, i+2));
+            if(twoDigit >= 10 && twoDigit <=26){
+                ways += f(i+2, s);
             }
         }
         return dp[i] = ways;
