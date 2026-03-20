7b3dafce48f78d82fbb465cf264a674a68ddcd64
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-20 22:19:08 +0530

    Time: 16 ms (34.76%) | Memory: 47.2 MB (97.32%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -12,18 +12,18 @@ public class Codec {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if(root == null) return "";
-        Queue<TreeNode> q = new LinkedList<>();
         StringBuilder res = new StringBuilder();
-        q.add(root);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
         while(!q.isEmpty()){
             TreeNode node = q.poll();
-            if(node == null){
+            if(node == null) {
                 res.append("null ");
                 continue;
-            } 
-            res.append(node.val + " ");
-            q.add(node.left);
-            q.add(node.right);   
+            }  
+            res.append(node.val + " ");  
+            q.offer(node.left);
+            q.offer(node.right);
         }
         return res.toString();
     }
@@ -34,18 +34,18 @@ public class Codec {
         Queue<TreeNode> q = new LinkedList<>();
         String values[] = data.split(" ");
         TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-        q.add(root);
+        q.offer(root);
         for(int i=1; i<values.length; i++){
             TreeNode parent = q.poll();
             if(!values[i].equals("null")){
                 TreeNode left = new TreeNode(Integer.parseInt(values[i]));
                 parent.left = left;
-                q.add(left);
+                q.offer(left);
             }
             if(!values[++i].equals("null")){
                 TreeNode right = new TreeNode(Integer.parseInt(values[i]));
                 parent.right = right;
-                q.add(right);
+                q.offer(right);
             }
         }
         return root;
