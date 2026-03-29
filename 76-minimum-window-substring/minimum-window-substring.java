cac159810c9c0d39c85b462c1b7fe83f29c2cf71
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-29 17:37:13 +0530

    Time: 3 ms (75.35%) | Memory: 45.2 MB (96.38%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,22 +3,24 @@ class Solution {
         int n = s.length(), m = t.length();
         if(n < m) return "";
         int freq[] = new int[128];
-        for(char c: t.toCharArray()) freq[c]++;
-        int minLen = Integer.MAX_VALUE, cut = 0, start = -1, left = 0, right = 0;
+        for(char c: t.toCharArray()){
+            freq[c]++;
+        }
+        int left = 0, minLen = Integer.MAX_VALUE, right = 0, st = -1, cut = 0;
         while(right < n){
             if(freq[s.charAt(right)] > 0) cut++;
-            freq[s.charAt(right)] --;
+            freq[s.charAt(right)]--;
             while(cut == m){
-                if(right - left+1 < minLen){
-                    minLen = right-left+1;
-                    start = left;
-                }
+               if(right - left + 1 < minLen){
+                minLen = right - left + 1;
+                st = left;
+            }
                 freq[s.charAt(left)]++;
                 if(freq[s.charAt(left)] > 0) cut--;
                 left++;
             }
             right++;
         }
-        return start == -1? "": s.substring(start, start + minLen);
+        return st == -1 ? "" : s.substring(st, st + minLen);
     }
 }
