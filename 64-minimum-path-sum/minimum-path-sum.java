427d5bc0927850365a87aa5727e156bc4d398509
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-03-29 13:54:56 +0530

    Time: 1 ms (99.77%) | Memory: 50.5 MB (17.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,17 +1,16 @@
 class Solution {
+    int dp[][];
     public int minPathSum(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int dp[][] = new int[m][n];
-        for(int i=0; i<m; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        return f(dp, grid, m-1, n-1);
+        int n = grid.length, m = grid[0].length;
+        dp = new int[n][m];
+        for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
+        return f(n-1, m-1, grid);
     }
-    private int f(int dp[][], int grid[][], int i, int j){
-        if(i==0 && j==0) return grid[0][0];
+    public int f(int i, int j, int grid[][]){
+        if(i==0 && j==0) return grid[i][j];
         if(dp[i][j] != -1) return dp[i][j];
-        int up = (i>0)? grid[i][j] + f(dp, grid, i-1, j): Integer.MAX_VALUE;
-        int left = (j>0)? grid[i][j] + f(dp, grid, i, j-1): Integer.MAX_VALUE;
+        int up = (i>0) ? grid[i][j] + f(i-1, j, grid): Integer.MAX_VALUE;
+        int left =(j>0)? grid[i][j] + f(i, j-1, grid): Integer.MAX_VALUE;
         return dp[i][j] = Math.min(up, left);
     }
 }
