4dd721dec3169e04e14733226f1e3d57553dc710
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-02 14:22:31 +0530

    Time: 19 ms (23.16%) | Memory: 57.1 MB (29.66%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,18 +1,20 @@
 class Solution {
+    int n;
+    int dp[][];
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int dp[][] = new int[n][amount+1];
+        n = coins.length;
+        dp = new int[n][amount+1];
         for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
-        return f(n-1, amount, coins, dp);
+        return f(n-1, coins, amount);
     }
-    public int f(int i, int amount, int coins[], int dp[][]){
+    public int f(int i, int coins[], int amount){
         if(amount == 0) return 1;
-        if(i<0) return 0; 
+        if(i < 0) return 0;
         if(dp[i][amount] != -1) return dp[i][amount];
-        int nottake = f(i-1, amount, coins, dp);
+        int nottake = f(i-1, coins, amount);
         int take = 0;
-        if(coins[i] <=amount){
-            take = f(i, amount - coins[i], coins, dp);
+        if(coins[i] <= amount){
+            take = f(i, coins, amount - coins[i]);
         }
         return dp[i][amount] = take + nottake;
     }
