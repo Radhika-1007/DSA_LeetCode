d9de8f0ee55652e0d8134184188549f24e1cb57e
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-02 11:58:41 +0530

    Time: 19 ms (70.51%) | Memory: 54.5 MB (45.46%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,19 +1,21 @@
 class Solution {
+    int n, m;
+    int dp[][];
     public int numDistinct(String s, String t) {
-        int n = s.length(), m = t.length();
-        int dp[][] = new int[n][m];
+        n = s.length(); m = t.length();
+        dp = new int[n][m];
         for(int i=0; i<n; i++) Arrays.fill(dp[i], -1);
-        return f(n-1, m-1, s, t, dp);
+        return f(n-1, m-1, s, t);
     }
-    private int f(int i, int j, String s, String t, int dp[][]){
+    public int f(int i, int j, String s, String t){
         if(j<0) return 1;
         if(i<0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
         if(s.charAt(i) == t.charAt(j)){
-            return dp[i][j] = f(i-1, j-1, s, t, dp) + f(i-1, j, s, t, dp);
+            return dp[i][j] = f(i-1, j-1, s, t) + f(i-1, j, s, t);
         }
         else{
-            return dp[i][j] = f(i-1, j, s, t, dp);
+            return dp[i][j] = f(i-1, j, s, t);
         }
     }
 }
