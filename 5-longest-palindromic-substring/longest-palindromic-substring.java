b0ab9f8aca08d5f550fa4770727a9679924e7699
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-04 18:32:28 +0530

    Time: 14 ms (92.95%) | Memory: 43.4 MB (84.66%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,24 @@
 class Solution {
+    int n;
     public String longestPalindrome(String s) {
-        int n = s.length();
-        int dp[][] = new int[n][n];
+        n = s.length();
+        int start = 0, end = 0;
         for(int i=0; i<n; i++){
-            Arrays.fill(dp[i], -1);
-        }
-        int st = 0;
-        int maxlen = 0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(isPalin(i, j, s, dp) == 1){
-                    if(j-i+1 > maxlen){
-                        maxlen = j-i+1;
-                        st = i;
-                    }
-                }
+            int len1 = f(i, i, s);
+            int len2 = f(i, i+1, s);
+            int len = Math.max(len1, len2);
+            if(len > end - start){
+                start = i - (len - 1)/2;
+                end = i + (len)/2;
             }
         }
-        return s.substring(st, st+maxlen);
-
+        return s.substring(start, end+1);
     }
-    private int isPalin(int i, int j, String s, int dp[][]){
-        if(i>j) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = isPalin(i+1, j-1, s, dp);
-        }    
-        else{
-            return dp[i][j] = 0;
+    public int f(int l, int r, String s){
+        while(l>=0 && r<n && s.charAt(l) == s.charAt(r)){
+            l--;
+            r++;
         }
-        
+        return r-l-1;
     }
 }
