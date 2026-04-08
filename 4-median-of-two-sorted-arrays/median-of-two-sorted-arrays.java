b1e0cdcf66bb8dce1dd2ac42ac68a0703188ef17
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-09 00:05:39 +0530

    Time: 1 ms (100.00%) | Memory: 48.5 MB (96.99%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,23 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length, n2 = nums2.length;
-        if(n1>n2) return findMedianSortedArrays(nums2, nums1);
-        int low = 0, high = n1;
-        int left = (n1+n2+1)/2;
+        int n = nums1.length, m = nums2.length;
+        if(n>m) return findMedianSortedArrays(nums2, nums1);
+        int low = 0, high = n;
         while(low <= high){
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE, r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
-            if(mid1 < n1) r1 = nums1[mid1];
-            if(mid2 < n2) r2 = nums2[mid2];
-            if(mid1 - 1 >= 0) l1 = nums1[mid1-1];
-            if(mid2 - 1 >= 0) l2 = nums2[mid2-1];
-            if(l1 <= r2 && l2 <= r1){
-                if((n1+n2)%2 != 0) return Math.max(l1, l2);
-                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+            int i= (low + high)/2;
+            int j = (n+m+1)/2 - i;
+            int Aleft = (i==0)? Integer.MIN_VALUE: nums1[i-1];
+            int Aright = (i==n)? Integer.MAX_VALUE: nums1[i];
+            int Bleft = (j==0)? Integer.MIN_VALUE: nums2[j-1];
+            int Bright = (j==m)? Integer.MAX_VALUE: nums2[j];
+            if(Aleft <= Bright && Bleft <= Aright){
+                if((n+m)%2==0){
+                    return (Math.max(Aleft, Bleft) + Math.min(Aright, Bright))/2.0;
+                }
+                else return Math.max(Aleft, Bleft);
             }
-            else if(l1 > r2) high = mid1 - 1;
-            else low = mid1 + 1;
+            else if(Aleft > Bright){
+                high = i-1;
+            }
+            else{
+                low = i+1;
+            }
         }
         return 0.0;
     }
