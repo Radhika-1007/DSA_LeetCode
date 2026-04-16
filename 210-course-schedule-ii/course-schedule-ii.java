1f3e85e730031747963570961c20e3f7381be3ab
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-16 11:02:49 +0530

    Time: 5 ms (83.51%) | Memory: 47.1 MB (33.92%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -2,30 +2,27 @@ class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i=0; i<numCourses; i++) adj.add(new ArrayList<>());
-        for(int it[]: prerequisites){
-            adj.get(it[1]).add(it[0]);
-        }
         int indegree[] = new int[numCourses];
-        for(int i=0; i<numCourses; i++){
-            for(int it: adj.get(i)){
-                indegree[it]++;
-            }
+        for(int edge[]: prerequisites){
+            adj.get(edge[1]).add(edge[0]);
+            indegree[edge[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
         for(int i=0; i<numCourses; i++){
-            if(indegree[i] == 0)q.add(i);
+            if(indegree[i] == 0) q.offer(i);
         }
-        int topo[] = new int[numCourses];
-        int i=0;
+        int count = 0;
+        int ans[] = new int[numCourses];
+        int idx = 0;
         while(!q.isEmpty()){
             int node = q.poll();
-            topo[i++] = node;
+            ans[idx++] = node;
+            count++;
             for(int it: adj.get(node)){
                 indegree[it]--;
-                if(indegree[it] == 0) q.add(it);
+                if(indegree[it] == 0) q.offer(it);
             }
         }
-        if(i == numCourses) return topo;
-        return new int[]{};
+        return (count == numCourses)? ans: new int[0];
     }
 }
