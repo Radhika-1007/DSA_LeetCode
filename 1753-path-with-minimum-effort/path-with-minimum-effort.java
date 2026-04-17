933145d22ba9127d0435d7073aa9f82210dd3cdf
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-17 11:51:20 +0530

    Time: 39 ms (84.63%) | Memory: 48.2 MB (8.51%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -9,31 +9,24 @@ class Tuple{
 class Solution {
     public int minimumEffortPath(int[][] heights) {
         int n = heights.length, m = heights[0].length;
-        int dir[][] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
-        int distance[][] = new int[n][m];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                distance[i][j] = Integer.MAX_VALUE;
-            }
-        }
-        distance[0][0] = 0;
-        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>((x,y) -> x.dist - y.dist);
-        pq.add(new Tuple(0, 0, 0));
+        int dist[][] = new int[n][m];
+        for(int i=0; i<n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        dist[0][0] = 0;
+        int dir[][] = {{-1,0}, {0,1}, {0, -1}, {1, 0}};
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.dist, b.dist));
+        pq.offer(new Tuple(0, 0, 0));
         while(!pq.isEmpty()){
-            int d = pq.peek().dist;
-            int r = pq.peek().row;
-            int c = pq.peek().col;
-            pq.remove();
+            Tuple t = pq.poll();
+            int d = t.dist, r = t.row, c = t.col;
             if(r == n-1 && c == m-1) return d;
             for(int i=0; i<4; i++){
-                int nr = r + dir[i][0];
-                int nc = c + dir[i][1];
+                int nr = r + dir[i][0], nc = c + dir[i][1];
                 if(nr>=0 && nr<n && nc>=0 && nc<m){
                     int effort = Math.abs(heights[nr][nc] - heights[r][c]);
                     int maxEffort = Math.max(d, effort);
-                    if(maxEffort < distance[nr][nc]){
-                        distance[nr][nc] = maxEffort;
-                        pq.add(new Tuple(maxEffort, nr, nc));
+                    if(maxEffort < dist[nr][nc]){
+                        dist[nr][nc] = maxEffort;
+                        pq.offer(new Tuple(dist[nr][nc], nr, nc));
                     }
                 }
             }
