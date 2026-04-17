e9b41e93998888e7b3c4674f19857c4fe42125a5
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-17 10:49:06 +0530

    Time: 15 ms (60.28%) | Memory: 47.5 MB (71.92%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -7,32 +7,31 @@ class Pair{
 }
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n = grid.length, m = grid[0].length;
-        if(grid[0][0] == 1 || grid[n-1][m-1] == 1) return -1;
-        int dir[][] = {{-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}};
-        int vis[][] = new int[n][m];
+        int n = grid.length;
+        int dir[][] = {{-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1}, {0,1}, {-1,1}};
+        int vis[][] = new int[n][n];
         Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(0,0));
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1) return -1;
+        q.offer(new Pair(0, 0));
         vis[0][0] = 1;
-        int count = 1;
+        int path = 1;
         while(!q.isEmpty()){
             int size = q.size();
-            for(int ele=0; ele<size; ele++){
-                int r = q.peek().row;
-                int c = q.peek().col;
-                q.poll();
-                if(r == n-1 && c == m-1) return count;
-                for(int i=0; i<8; i++){
-                    int nr = r + dir[i][0];
-                    int nc = c +  dir[i][1];
-                    if(nr>=0 && nr<n && nc>=0 && nc<m && vis[nr][nc] == 0 && grid[nr][nc] == 0){
+            for(int i=0; i<size; i++){
+                Pair p = q.poll();
+                int r = p.row;
+                int c = p.col;
+                if(r == n-1 && c == n-1) return path;
+                for(int d=0; d<8; d++){
+                    int nr = dir[d][0] + r;
+                    int nc = dir[d][1] + c;
+                    if(nr>=0 && nr<n && nc>=0 && nc<n && grid[nr][nc] == 0 && vis[nr][nc] == 0){
                         q.offer(new Pair(nr, nc));
                         vis[nr][nc] = 1;
                     }
                 }
             }
-            count++;
-
+             path++;
         }
         return -1;
     }
