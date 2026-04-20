07ebc24486724ebf1d9ba1ec76641d411317d1f6
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-20 17:33:34 +0530

    Time: 637 ms (85.22%) | Memory: 63.4 MB (38.26%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,46 +1,40 @@
 class Solution {
-    int n, total;
+    int n, target, total, m;
     public int minimumDifference(int[] nums) {
-        n = nums.length; total = 0; int m = n/2;
-        for(int x: nums) total += x;
+        n = nums.length; total = 0; m = n/2;
+        for(int num: nums) total += num;
         int leftArr[] = Arrays.copyOfRange(nums, 0, m);
         int rightArr[] = Arrays.copyOfRange(nums, m, n);
-        List<List<Integer>> left = new ArrayList<>();
-        List<List<Integer>> right = new ArrayList<>();
-
+        ArrayList<ArrayList<Integer>> left = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> right = new ArrayList<>();
         for(int i=0; i<=m; i++){
             left.add(new ArrayList<>());
             right.add(new ArrayList<>());
         }
-        gen(0,0,0,leftArr,left);
-        gen(0,0,0,rightArr,right);
-        for(int i=0; i<=m; i++){
-            Collections.sort(right.get(i));
-        }
+        gen(0, 0, 0, leftArr, left);
+        gen(0, 0, 0, rightArr, right);
+        for(int i=0; i<=m; i++) Collections.sort(right.get(i));
         int ans = Integer.MAX_VALUE;
-        for(int k=0;k<=m;k++){
+        for(int k=0; k<=m; k++){
             for(int s1: left.get(k)){
-                List<Integer> rlist = right.get(m-k);
-
-                int target = total/2 - s1;
-                int idx = Collections.binarySearch(rlist,target);
-                if(idx<0) idx = -idx-1;
-
-                if(idx<rlist.size()){
-                    int sum = s1 + rlist.get(idx);
-                    ans = Math.min(ans,
-                        Math.abs(total-2*sum));
+                ArrayList<Integer> rList = right.get(m-k);
+                target = total/2 - s1;
+                int idx = Collections.binarySearch(rList, target);
+                if(idx < 0) idx = -idx - 1;
+                if(idx < rList.size()){
+                    int sum = s1 + rList.get(idx);
+                    ans = Math.min(ans, Math.abs(total - 2*sum));
                 }
-                if(idx>0){
-                    int sum = s1 + rlist.get(idx-1);
-                    ans = Math.min(ans,
-                        Math.abs(total-2*sum));
+                if(idx > 0){
+                    int sum = s1 + rList.get(idx-1);
+                    ans = Math.min(ans, Math.abs(total - 2*sum));
                 }
             }
+
         }
         return ans;
     }
-    public void gen(int i, int pick, int sum, int arr[], List<List<Integer>> res){
+    public void gen(int i, int pick, int sum, int arr[], ArrayList<ArrayList<Integer>> res){
         if(i==arr.length){
             res.get(pick).add(sum);
             return;
