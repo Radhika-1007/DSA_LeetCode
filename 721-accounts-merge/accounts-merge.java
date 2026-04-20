9c8a280d5c95ca2ad827159ea69fb21bb2f5b52b
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-20 11:46:29 +0530

    Time: 28 ms (92.35%) | Memory: 49.5 MB (69.22%) - LeetSync

--- 721-accounts-merge/accounts-merge.java
+++ 721-accounts-merge/accounts-merge.java
@@ -1,54 +1,37 @@
-class DisjointSet{
-    List<Integer> size = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    public DisjointSet(int n){
-        for(int i=0; i<=n; i++){
-            size.add(1);
-            parent.add(i);
-        }
-    }
-    public int findUpar(int node){
-        if(node == parent.get(node)) return node;
-        int ulp = findUpar(parent.get(node));
-        parent.set(node, ulp);
-        return parent.get(node);
-    }
-    public void unionSize(int u, int v){
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
-        if(ulp_u == ulp_v) return;
-        if(size.get(ulp_u) > size.get(ulp_v)){
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
-        }
-        else{
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
-        }
-    }
-}
 class Solution {
+    class DSU{
+        int parent[];
+        DSU(int n){
+            parent = new int[n];
+            for(int i=0; i<n; i++) parent[i] = i;
+        }
+        int find(int u){
+            if(parent[u] == u) return u;
+            return parent[u] = find(parent[u]);
+        }
+        void union(int a, int b){
+            int pa = find(a);
+            int pb = find(b);
+            if(pa != pb) parent[pa] = pb;
+        }
+    }
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n = accounts.size();
-        DisjointSet ds = new DisjointSet(n);
-        HashMap<String, Integer> mp = new HashMap<>();
+        DSU ds = new DSU(n);
+        Map<String, Integer> mp = new HashMap<>();
         for(int i=0; i<n; i++){
             for(int j=1; j<accounts.get(i).size(); j++){
                 String mail = accounts.get(i).get(j);
-                if(!mp.containsKey(mail)){
-                    mp.put(mail, i);
-                }
-                else{
-                    ds.unionSize(i, mp.get(mail));
-                }
+                if(!mp.containsKey(mail)) mp.put(mail, i);
+                else ds.union(mp.get(mail), i);
             }
         }
         List<List<String>> merged = new ArrayList<>();
         for(int i=0; i<n; i++) merged.add(new ArrayList<>());
         for(Map.Entry<String, Integer> it: mp.entrySet()){
             String mail = it.getKey();
-            int node = ds.findUpar(it.getValue());
-            merged.get(node).add(mail);  
+            int node = ds.find(it.getValue());
+            merged.get(node).add(mail);
         }
         List<List<String>> ans = new ArrayList<>();
         for(int i=0; i<n; i++){
