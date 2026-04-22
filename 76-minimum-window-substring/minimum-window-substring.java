ce50ba2ba7fdb2283e2c8111c40106f1967ad8af
Radhika-1007 <165660562+Radhika-1007@users.noreply.github.com>
2026-04-22 12:48:19 +0530

    Time: 2 ms (99.95%) | Memory: 45.6 MB (79.89%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,26 +1,24 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int n = s.length(), m = t.length();
-        if(n < m) return "";
-        int freq[] = new int[128];
-        for(char c: t.toCharArray()){
-            freq[c]++;
-        }
-        int left = 0, minLen = Integer.MAX_VALUE, right = 0, st = -1, cut = 0;
+        int n = s.length(), m = t.length(), st = -1, left = 0, minLen = Integer.MAX_VALUE, right = 0, cut = 0;
+        if(n<m) return "";
+        int freq[] = new int[256];
+        for(char c : t.toCharArray()) freq[c]++;
         while(right < n){
-            if(freq[s.charAt(right)] > 0) cut++;
-            freq[s.charAt(right)]--;
+            char ch = s.charAt(right);
+            if(freq[ch] > 0) cut++;
+            freq[ch]--;
             while(cut == m){
-               if(right - left + 1 < minLen){
-                minLen = right - left + 1;
-                st = left;
-            }
+               if(right - left + 1< minLen){
+                    minLen = right - left + 1;
+                    st = left;
+               }
                 freq[s.charAt(left)]++;
                 if(freq[s.charAt(left)] > 0) cut--;
                 left++;
             }
             right++;
         }
-        return st == -1 ? "" : s.substring(st, st + minLen);
+        return (st == -1)? "": s.substring(st, st + minLen);
     }
 }
